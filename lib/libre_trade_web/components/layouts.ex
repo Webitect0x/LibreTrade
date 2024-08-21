370b2e128418e70b2e6f70747094d3164836d8b2
Webitect <webitect@protonmail.com>
2024-08-20 19:40:26 -0700

    Initial app

---
+++ lib/libre_trade_web/components/layouts.ex
@@ -0,0 +1,15 @@
+defmodule LibreTradeWeb.Layouts do
+  @moduledoc """
+  This module holds different layouts used by your application.
+
+  See the `layouts` directory for all templates available.
+  The "root" layout is a skeleton rendered as part of the
+  application router. The "app" layout is set as the default
+  layout on both `use LibreTradeWeb, :controller` and
+  `use LibreTradeWeb, :live_view`.
+  """
+  use LibreTradeWeb, :html
+  alias LibreTradeWeb.Components.Navbar
+
+  embed_templates "layouts/*"
+end
